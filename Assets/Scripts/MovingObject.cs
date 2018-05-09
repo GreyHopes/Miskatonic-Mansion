d8d1b5fa16573480fc499c8b8afcf8eef0791409
Fab <fab.mckay@gmail.com>
2018-05-09 12:45:38 +0200

    I like to move it, move it !

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MovingObject : MonoBehaviour {
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    public LayerMask layer;
+    public Tilemap wallTilemap;
+
+	// Use this for initialization
+	protected virtual void Start ()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+
+    protected void Move (int xDir,int yDir)
+    {
+        print(transform.position);
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+      
+
+        if (!getCell(wallTilemap,end))
+            rb2D.MovePosition(end);
+    }
+
+    private TileBase getCell(Tilemap tilemap, Vector2 cellWorldPos)
+    {
+        return tilemap.GetTile(tilemap.WorldToCell(cellWorldPos));
+    }
+    private bool hasTile(Tilemap tilemap, Vector2 cellWorldPos)
+    {
+        return tilemap.HasTile(tilemap.WorldToCell(cellWorldPos));
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+
+	// Use this for initialization
+	protected override void Start ()
+    {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { 
+            Move(0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(0,-1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(-1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(1,0);
+        }
+
+    }
+}
