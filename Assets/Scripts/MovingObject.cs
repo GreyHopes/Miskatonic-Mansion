09d70d56df0253beaed7542fd01902d5bf7f4210
Fab <fab.mckay@gmail.com>
2018-05-09 12:49:01 +0200

    Abstract is better

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class MovingObject : MonoBehaviour {
+public abstract class MovingObject : MonoBehaviour {
 
     private BoxCollider2D boxCollider;
-    private Rigidbody2D rb2D;
+    protected Rigidbody2D rb2D;
     public LayerMask layer;
     public Tilemap wallTilemap;
 
@@ -17,23 +17,13 @@ public class MovingObject : MonoBehaviour {
         rb2D = GetComponent<Rigidbody2D>();
 	}
 
-    protected void Move (int xDir,int yDir)
-    {
-        print(transform.position);
-        Vector2 start = transform.position;
-        Vector2 end = start + new Vector2(xDir, yDir);
+    protected abstract void Move(int xDir, int yDir);
 
-      
-
-        if (!getCell(wallTilemap,end))
-            rb2D.MovePosition(end);
-    }
-
-    private TileBase getCell(Tilemap tilemap, Vector2 cellWorldPos)
+    protected TileBase getCell(Tilemap tilemap, Vector2 cellWorldPos)
     {
         return tilemap.GetTile(tilemap.WorldToCell(cellWorldPos));
     }
-    private bool hasTile(Tilemap tilemap, Vector2 cellWorldPos)
+    protected bool hasTile(Tilemap tilemap, Vector2 cellWorldPos)
     {
         return tilemap.HasTile(tilemap.WorldToCell(cellWorldPos));
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MovingObject {
+   
 
-	// Use this for initialization
-	protected override void Start ()
+    // Use this for initialization
+    protected override void Start ()
     {
         base.Start();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    protected override void Move(int xDir, int yDir)
+    {
+        print(transform.position);
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+
+
+        if (!getCell(wallTilemap, end))
+            rb2D.MovePosition(end);
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
-        { 
+        {
             Move(0, 1);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Move(0,-1);
+            Move(0, -1);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -27,8 +40,10 @@ public class Player : MovingObject {
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Move(1,0);
+            Move(1, 0);
         }
-
     }
+
+
+
 }
