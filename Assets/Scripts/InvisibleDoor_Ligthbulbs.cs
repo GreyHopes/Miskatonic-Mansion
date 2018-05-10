3a76ab8b7e507e3831a07048139715d211f97d35
Fab <fab.mckay@gmail.com>
2018-05-10 12:52:40 +0200

    More Clarity

--- Assets/Scripts/InvisibleDoor_Ligthbulbs.cs
+++ Assets/Scripts/InvisibleDoor_Ligthbulbs.cs
@@ -2,29 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InvisibleDoor : MonoBehaviour {
+public class InvisibleDoor_Ligthbulbs : MonoBehaviour {
 
     public List<Lightbulb> controlling_bulbs;
     private BoxCollider2D boxCollider;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         bool test = true;
         int i = 0;
-        while(test && i < controlling_bulbs.Count)
+        while (test && i < controlling_bulbs.Count)
         {
             test = test && controlling_bulbs[i].state;
             i++;
         }
 
-        if(test)
+        if (test)
         {
             boxCollider.enabled = false;
         }
@@ -32,5 +32,5 @@ public class InvisibleDoor : MonoBehaviour {
         {
             boxCollider.enabled = true;
         }
-	}
+    }
 }
